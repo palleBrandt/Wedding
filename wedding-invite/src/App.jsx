cef8da3b9522b451b3118ce6f89a7c993caabbfe
Lukas <lupa@itu.dk>
2026-05-17 20:23:39 +0200

    Initial commit

---
+++ wedding-invite/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div style={{ scrollBehavior: "smooth" }}>
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ wedding-invite/src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <section style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", background: "#fce4ec" }}>
+      <h1>We're Getting Married!</h1>
+    </section>
+  );
+}
